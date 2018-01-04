86321c715245a6f60f6abe36bdd5a0edae1866ff
zhengkai <568230747@qq.com>
2018-01-04 10:49:40 +0800

    删除依赖文件

--- webpack.common.js
+++ webpack.common.js
@@ -48,8 +48,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['babel-preset-env'],
-                        plugins: ['babel-plugin-transform-runtime']
+                        presets: ['env']
                     }
                 }
             },
